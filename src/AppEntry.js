9002281ac39e0ac846384dcfca72f2ffe60e00a0
jakub007d <dr.kubko@gmail.com>
2026-05-06 11:22:59 +0200

    fix: Federated modules should not create own touter

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
-const AppEntry = () => (
-  <Router>
-    <App />
-  </Router>
-);
+const AppEntry = () => <App />;
 
 export default AppEntry;
